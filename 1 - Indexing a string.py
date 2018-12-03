62357c564fe8f30ceef8fc4ae7c5ef7466407e85
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-03 20:54:47 +0000

    Update and rename 1 - Indexing a string to 1 - Indexing a string.py
    adding comments

---
+++ 1 - Indexing a string.py
@@ -0,0 +1,11 @@
+a="hello, world"
+
+# this way print in different lines
+for i in [1,4,7]:
+    print (a[i]) # first time i did a mistake and put () 
+                 # for index instead of [] 
+                
+
+#Alternative way to print out in the same line
+print (a[1],a[4],a[7])  
+    
